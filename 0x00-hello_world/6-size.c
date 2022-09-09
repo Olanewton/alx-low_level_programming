2cf094ca69c7b7a7e4d1134024d9bd1f8eb42601
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-09 10:13:42 -0700

    size correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+
 /**
-*  *main -is the entry point for the program.
-*   *
-*    *Return:zero if no error, Non-zero value  if errors.   
-*     */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-		printf("Size of a char: %ld byte(s)\n", sizeof(char));
-			printf("Size of an int: %ld byte(s)\n", sizeof(int));
-				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %ld byte(s)\n", sizeof(float));
-							return (0);
+	char c;
+	int a;
+	long b;
+	long long d;
+	float k;
+	
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", sizeof(k));
+	return (0);
 }
