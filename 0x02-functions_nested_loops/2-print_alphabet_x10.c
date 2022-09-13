a37bf2c975a35be8e728964fccae01a993fb5762
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-13 15:05:39 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function to print azc 10 times 
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int m;
+	char n;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
