3ee615566805f3a8faead2546b68615fa59b7e5e
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-13 10:58:13 -0700

    putchar final correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@ int main(void)
 
 {
 	_putchar('_');
-	_putchar('a');
-	_putchar('c');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('p');
+	_putchar('c');
 	_putchar('h');
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
