3fe1f061f8c828b4e7ab959ff3ad88c2f610ac97
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-13 09:42:59 -0700

    Task 0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include 'main.h'
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('a');
+	-putchar('c');
+	-putchar('h');
+	-putchar('r');
+	-putchar('t');
+	-putchar('u');
+	-putchar('\n');
+
+	return (0);
+}
