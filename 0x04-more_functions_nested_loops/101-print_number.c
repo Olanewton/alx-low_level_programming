50672036b3315494a552236721d91afc58ddb000
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-15 21:09:20 -0700

    101-print_number update1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- *print_number - prints a number
- *@n: The number to print
- */
+* print_number - prints a number
+* @n: The number to print
+*/
 
 void print_number(int n)
 {
