7bd515598bd21cee121eb270321af7812483e593
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-22 13:03:27 -0700

    0-strcat update1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -16,4 +15,4 @@ char *add_strings(charchar *n1, char *n2, char *r, int size_r);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-#endif /*MAIN_H*/
+#endif
