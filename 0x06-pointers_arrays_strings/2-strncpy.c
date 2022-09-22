115f343d5d2417eb55dca9261c115ccecd1eecc7
Olanewton <Jolakanmi01@gmai.lcom>
2022-09-21 22:43:05 -0700

    2-strncpy correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
-*  _strncpy ->  a function that copies a string
+*  _strncpy ->  a function that copies
 *  @dest: parameter1
 *  @src: parameter2
 *  @n: parameter3
-*  Returne: string
+*  Return: string
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	_strncpy(dest, src, n);
+	strncpy(dest, src, n);
 	return (dest);
 }
