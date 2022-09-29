877c9012654a8a3d716193af72321e7c60aab36b
Olanewton <jolakanmi01@gmail.com>
2022-09-30 00:33:56 +0100

    Task 5-sqrt_recursion update5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,41 @@
 #include "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 /**
- * _pow_recursion - a function that returns natural square root of a number
- * @x: The number to be raised
- * @y: The power
- *
- * Return: The value of x raised to the power of y
+ * find_sqrt - functions to find natural square root of an inputted number
+ * @num: The number to find the square root of
+ * @root: The root to be tested
+ * Return: If the number has a natural square root - the square root
+ * If the number does not have a natural square root - -1
+ * */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number to return the square root of
+ * 
+ * Return: If n has a natural square root - the natural square root of n
+ * If n does not have a natural square root - -1
  */
 int _sqrt_recursion(int n)
 {
-	int result = x;
+	int root = 0;
 
-	if (y < 0)
+	if (n < 0)
 		return (-1);
 
-	else if (y == 0)
+	if (n == 1)
 		return (1);
 
-	result *= _pow_recursion(x, y -1);
-
-	return (result);
+	return (find_sqrt(n, root));
 }
