172d8f4dec487914fad7d4b682ced4bcef842f5b
Olanewton <jolakanmi01@gmail.com>
2022-09-29 22:14:53 +0100

    Task 5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that returns natural square root of a number
+ * @x: The number to be raised
+ * @y: The power
+ *
+ * Return: The value of x raised to the power of y
+ */
+int _sqrt_recursion(int n);
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
