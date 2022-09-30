d85016bc8d06df7cc6b72feb3fa94367252ad2a6
Olanewton <jolakanmi01@gmail.com>
2022-09-30 20:57:17 +0100

    Task 2-args update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Print the name of the program
+ * main - program that prints all arguments
  * @argc: Count arguments
  * @argv: Arguments
  *
@@ -10,17 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
+
 /*Declaring variables*/
 int count = 0;
 
 if (argc > 0)
 {
 /*WHILE - Print each arguments*/
-
 while (count < argc)
 {
-	printf("%s\n", argv[count]);
-	count++;
+printf("%s\n", argv[count]);
+count++;
 }
 }
 Return (0);
