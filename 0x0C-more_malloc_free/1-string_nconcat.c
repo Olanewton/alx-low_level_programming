73e03d568db34acc6c36e3972453a0df24111ccb
Olanewton <Jolakanmi01@gmai.lcom>
2022-10-06 12:58:34 -0700

    Task 1-string_nconcat.c update4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -46,7 +46,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr[i] = s1[i];
 	for (j = 0; j < num; j++)
 	ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
-	
+	ptr[i + j] = '\0';	
 	return (ptr);
 }
