af8e6d54958bcc9e7c53395673438bc972c83c18
Olanewton <Jolakanmi01@gmai.lcom>
2022-10-06 17:31:08 -0700

    Task 1 update6

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,62 +10,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size1 = 0, size2 = 0, i;
+char *p;
+unsigned int size1 = 0, size2 = 0, i;
 
-	if (s1 == NULL)
-		s1 = "";
+if (s1 == NULL)
+s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
-<<<<<<< HEAD
-	if (num < 0) /* account for negative n bytes */
-		return (NULL);
-	if (num >= _strlen(s2) /* account for n too big */
-			num = _strlen(s2);
+if (s2 == NULL)
+s2 = "";
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+while (s1[size1] != '\0')
+{
+size1++;
+}
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-	if (ptr == NULL)
-	return (NULL);
+while (s2[size2] != '\0')
+{
+size2++;
+}
 
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-	ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-	ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';	
-	return (ptr);
-=======
+if (n > size2)
+n = size2;
+p = malloc((size1 + n + 1) * sizeof(char));
 
-	while (s1[size1] != '\0')
-	{
-		size1++;
-	}
+if (p == NULL)
+return (0);
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
+for (i = 0; i < size1; i++)
+{
+p[i] = s1[i];
+}
 
-	if (n > size2)
-	n = size2;
-	p = malloc((size1 + n + 1) * sizeof(char));
-
-	if (p == NULL)
-		return (0);
-
-	for (i = 0; i < size1; i++)
-	{
-		p[i] = s1[i];
-	}
-
-	for (; i < (size1 + n); i++)
-	{
-		p[i] = s2[i - size1];
-	}
-	p[i] = '\0';
+for (; i < (size1 + n); i++)
+{
+p[i] = s2[i - size1];
+}
+p[i] = '\0';
 
 return (p);
->>>>>>> 232c56845cb2f62c05f09b16f50a5e3ad98f2a20
 }
