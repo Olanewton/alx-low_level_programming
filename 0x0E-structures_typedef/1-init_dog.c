766a1fb600db215626e8eb1b303ab5632368bad1
Olanewton <Jolakanmi01@gmai.lcom>
2022-10-10 15:29:36 -0700

    Task 1-init_dog.c updated 6

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -6,15 +7,16 @@
  * @name: pointer to char name's dog
  * @age: age's dog
  * @owner: pointer to char owner's dog
- * Return: No.
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
